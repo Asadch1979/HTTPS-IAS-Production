d4bae67fd1c54f3c2ab2632377f48fd7fdf2b639
Asadch1979 <asadali3000@hotmail.com>
2026-01-25 18:34:49 +0500

    Improve mandatory password change UX

--- AIS/wwwroot/js/home-change-password.js
+++ AIS/wwwroot/js/home-change-password.js
@@ -7,17 +7,69 @@
         window.location.href = g_asiBaseURL + "/Home/Index";
     }
 
+    function getLoginUrl() {
+        return g_asiBaseURL + "/Login/Index";
+    }
+
+    function setProcessingState(isProcessing, shouldEnableOnStop) {
+        var submitButton = document.getElementById('changePasswordSubmit');
+        var spinner = document.getElementById('changePasswordSpinner');
+        var submitText = document.getElementById('changePasswordSubmitText');
+
+        if (spinner) {
+            spinner.classList.toggle('d-none', !isProcessing);
+        }
+
+        if (submitText) {
+            submitText.textContent = isProcessing ? 'Processing...' : 'Submit';
+        }
+
+        if (submitButton) {
+            if (isProcessing) {
+                submitButton.disabled = true;
+            } else {
+                submitButton.disabled = !shouldEnableOnStop;
+            }
+        }
+    }
+
+    function isAuthFailureResponse(xhr, textStatus) {
+        if (!xhr) {
+            return false;
+        }
+
+        if (xhr.status === 401 || xhr.status === 403) {
+            return true;
+        }
+
+        if (textStatus === 'parsererror') {
+            return true;
+        }
+
+        var responseText = xhr.responseText || '';
+        return responseText.indexOf('Login') !== -1 || responseText.indexOf('login') !== -1;
+    }
+
     function onSubmitChangePassword() {
+        var submitButton = document.getElementById('changePasswordSubmit');
+        if (submitButton && submitButton.disabled) {
+            return;
+        }
+
+        setProcessingState(true);
+
         const newPassword = $('#inputNewPassword').val();
         const confirmPassword = $('#inputConfirmPassword').val();
 
         if (confirmPassword !== newPassword) {
             alert('New Passowrd and Confirm Password does not match');
+            setProcessingState(false, true);
             return;
         }
 
         if (!checkPassword(newPassword)) {
             alert('Password does not meet security requirements.');
+            setProcessingState(false, true);
             return false;
         }
 
@@ -31,23 +83,26 @@
             },
             cache: false,
             success: function (data) {
-                if (data && data.Status) {
-                    var target = (data && data.RedirectUrl) ? data.RedirectUrl : (g_asiBaseURL + "/Login/Index");
-                    alert('Password Changed');
-                    onAlertCallback(function () {
-                        window.location.href = target;
-                    });
+                var isSuccess = data && (data.Success === true || data.Status === true);
+                if (isSuccess) {
+                    var target = (data && data.RedirectUrl) ? data.RedirectUrl : getLoginUrl();
+                    setProcessingState(false, false);
+                    showApiAlert(data, "Password Changed");
+                    window.location.href = target;
                 } else {
+                    setProcessingState(false, true);
                     showApiAlert(data, "Unable to change password. Please try again.");
                 }
             },
-            error: function (xhr) {
-                if (xhr && xhr.status === 401) {
-                    showApiAlertFromXhr(xhr, xhr.status, getErrorReferenceIdFromXhr(xhr), 'Password change session expired, login again.');
-                    window.location.href = g_asiBaseURL + "/Login/Index";
+            error: function (xhr, textStatus) {
+                if (isAuthFailureResponse(xhr, textStatus)) {
+                    setProcessingState(false, true);
+                    showApiAlertFromXhr(xhr, xhr && xhr.status ? xhr.status : null, getErrorReferenceIdFromXhr(xhr), 'Session expired, please login again.');
+                    window.location.href = getLoginUrl();
                     return;
                 }
 
+                setProcessingState(false, true);
                 showApiAlertFromXhr(xhr, xhr ? xhr.status : null, getErrorReferenceIdFromXhr(xhr), "Unable to change password. Please try again.");
             },
             dataType: "json",
