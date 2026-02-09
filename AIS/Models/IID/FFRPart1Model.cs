c7b5040fbad513963873ce2f9449fad668f764fb
Asadch1979 <asadali3000@hotmail.com>
2026-02-09 15:21:45 +0500

    Update IID complaint and FFR workflows

---
+++ AIS/Models/IID/FFRPart1Model.cs
@@ -0,0 +1,65 @@
+using AIS.Validation;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+
+namespace AIS.Models.IID
+    {
+    public class FFRPart1Model
+        {
+        public int ComplaintId { get; set; }
+        public int FfrId { get; set; }
+        [PlainText]
+        public string PertainsTo { get; set; }
+        [PlainText]
+        public string FieldType { get; set; }
+        public int? HOUnitTypeId { get; set; }
+        public int? HOUnitId { get; set; }
+        public int? RegionId { get; set; }
+        public int? BranchId { get; set; }
+        [PlainText]
+        public string Source { get; set; }
+        [PlainText]
+        public string SourceOtherText { get; set; }
+        [PlainText]
+        public string Nature { get; set; }
+        [PlainText]
+        public string ReferenceNo { get; set; }
+        [PlainText]
+        public string FFRDate { get; set; }
+        [PlainText]
+        public string IncidentDate { get; set; }
+        [PlainText]
+        public string IncidentVenue { get; set; }
+        [PlainText]
+        public string IncidentNarrative { get; set; }
+        [PlainText]
+        public string ComplainantName { get; set; }
+        [PlainText]
+        public string ComplainantCNIC { get; set; }
+        [PlainText]
+        public string AccountNo { get; set; }
+        [PlainText]
+        public string ComplainantMobile { get; set; }
+        [PlainText]
+        public string ComplainantAddress { get; set; }
+        [PlainText]
+        public string MainAccused { get; set; }
+        [PlainText]
+        public string CoAccused { get; set; }
+        [PlainText]
+        public string Accusations { get; set; }
+        [PlainText]
+        public string ApproachAdopted { get; set; }
+        [PlainText]
+        public string RecordScrutinized { get; set; }
+        [PlainText]
+        public string RootCause { get; set; }
+        [PlainText]
+        public string KeyFindings { get; set; }
+        [PlainText]
+        public string ClearRecommendations { get; set; }
+        public List<IFormFile> Attachments { get; set; }
+        [PlainText]
+        public string UploadedAttachments { get; set; }
+        }
+    }
