2bf80b539fe19df657be04612ad52ffb5ce9dee5
Asadch1979 <asadali3000@hotmail.com>
2026-02-23 14:16:00 +0500

    Add IID findings-by-accusation API endpoints and DB mappings

--- AIS/Models/IID/InquiryReport/IidInqFindingsRecommRow.cs
+++ AIS/Models/IID/InquiryReport/IidInqFindingsRecommRow.cs
@@ -5,6 +5,7 @@ namespace AIS.Models.IID.InquiryReport
     public class IidInqFindingsRecommRow
         {
         public long ComplaintId { get; set; }
+        public long AccusationId { get; set; }
         public string FindingText { get; set; }
         public string RecommendationText { get; set; }
         public string Ppno { get; set; }
