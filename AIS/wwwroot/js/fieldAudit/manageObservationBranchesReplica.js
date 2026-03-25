afcafe7f9a3133865aea84bbb9ef81bfbf44c40e
Asadch1979 <asadali3000@hotmail.com>
2026-03-25 10:00:31 +0500

    Revert "Stabilizing AR_DASHBoard"
    This reverts commit 3d8535572431bc819b564f420d14ed3a41ced991.

--- AIS/wwwroot/js/fieldAudit/manageObservationBranchesReplica.js
+++ AIS/wwwroot/js/fieldAudit/manageObservationBranchesReplica.js
@@ -24,7 +24,7 @@ function getPageData() {
     var g_currentStatus = 0;
     var g_obsList = [];
     var g_selectedRiskId = 0;
-    var pageData = getPageData();
+    const pageData = getPageData();
     var g_annexList = pageData.AnnexList || [];
     var g_processId = 0;
     var g_subProcessId = 0;
@@ -33,53 +33,27 @@ function getPageData() {
     var g_dsa = "";
     var g_tablePage = 0;
     var g_scrollPos = 0;
-
-    function resolveCurrentEngagementId() {
-        var hiddenValue = $('#engIdHidden').val();
-        var localValue = $('#fieldAuditManageObservationBranchesReplica').attr('data-eng-id');
-        var hostValue = $('#fieldAuditStepHost').attr('data-eng-id');
-        var rawValue = hiddenValue || localValue || hostValue || pageData.EngagementId || 0;
-        var parsed = parseInt(rawValue, 10);
-        return Number.isNaN(parsed) ? 0 : parsed;
-    }
-
-    function syncEngagementContext() {
-        var engId = resolveCurrentEngagementId();
-        $('#engIdHidden').val(engId || '');
-        $('#fieldAuditManageObservationBranchesReplica').attr('data-eng-id', engId || '');
-        return engId;
-    }
-
     function preserveTablePosition() {
         g_scrollPos = $('html').scrollTop();
         if ($.fn.DataTable.isDataTable('#manageObsPanel')) {
             g_tablePage = $('#manageObsPanel').DataTable().page();
         }
     }
-    function initManageObservationBranches() {
-        var root = document.getElementById('fieldAuditManageObservationBranchesReplica');
-        if (!root || root.getAttribute('data-initialized') === '1') {
-            return;
-        }
-
-        root.setAttribute('data-initialized', '1');
+    $(document).ready(function () {
         console.log("Loaded manage_observations_branches JS", { g_obsId, g_entityID });
         var entName = $('#manageObsPanel tbody .entity_name_field:first').text();
         $('#entityNameField').val(entName);
         var periodName = $('#manageObsPanel tbody .period_name_field:first').text();
         $('#auditPeriodNameField').val(periodName);
 
-        if (!document.querySelector('#updateMemoModel .richText-editor')) {
-            $('#updateMemoContent').richText({
-                imageUpload: false,
-                fileUpload: false,
-                videoEmbed: false,
-                urls: false
-            });
-        }
-
-        $('#updateMemo_annex').off('change.manageObservationBranches').on('change.manageObservationBranches', updateRiskDisplay);
-        var engId = syncEngagementContext();
+        $('#updateMemoContent').richText({
+            imageUpload: false,
+            fileUpload: false,
+            videoEmbed: false,
+            urls: false
+        });
+        $('#updateMemo_annex').on('change', updateRiskDisplay);
+        const engId = parseInt($('#engIdHidden').val() || 0);
         respSectionUpdate = initResponsibilitySection({
             tableSelector: '#update_listofRespPersons',
             changesTableSelector: '#c_update_listofRespPersons',
@@ -93,7 +67,7 @@ function getPageData() {
         });
 
         getEntityObservation();
-    }
+    });
     function reloadLocation() {
         getEntityObservation();
     }
@@ -158,7 +132,7 @@ function getPageData() {
         rebuildManageObservationTableBody();
         renderMessageRow('Please wait...', 'manage-obs-loading');
 
-        var selectedEngId = syncEngagementContext();
+        var selectedEngId = parseInt($('#engIdHidden').val() || 0);
         if (respSectionUpdate) {
             respSectionUpdate.updateContext({ engId: selectedEngId });
         }
@@ -218,7 +192,7 @@ function getPageData() {
             return;
         }
 
-        var engId = syncEngagementContext();
+        var engId = $('#engIdHidden').val();
         if (!engId || engId <= 0) {
             alert('Engagement id is required.');
             return;
@@ -374,7 +348,7 @@ function getPageData() {
                 }
 
                 initReferenceSection(obs_id, false, '#updateMemoModel #referenceSection');
-                var engId = syncEngagementContext();
+                var engId = parseInt($('#engIdHidden').val() || 0);
                 respSectionUpdate.updateContext({ newParaId: obs_id, engId: engId });
                 showActionButtons();
             },
@@ -535,7 +509,7 @@ function getPageData() {
                 data: {
                     'OBS_ID': g_obsId,
                     'ENTITY_ID': g_entityID,
-                    'ENG_ID': syncEngagementContext(),
+                    'ENG_ID': $('#engIdHidden').val(),
                     "RespDSAModel":dsaArr
                 },
                 cache: false,
@@ -671,13 +645,4 @@ function getPageData() {
 
         const blob = new Blob(byteArrays, { type: contentType });
         return blob;
-    }
-
-    window.fieldAuditStepInitializers = window.fieldAuditStepInitializers || {};
-    window.fieldAuditStepInitializers.MANAGE_OBSERVATION_BRANCHES = initManageObservationBranches;
-
-    if (document.readyState !== 'loading') {
-        initManageObservationBranches();
-    } else {
-        $(initManageObservationBranches);
     }
